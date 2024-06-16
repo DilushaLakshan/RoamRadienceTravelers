0de47e161b4022a326bd15f1e0e56ded5bb5cfcf
DilushaLakshan <dilushalakshankumara0@gmail.com>
2024-06-16 16:43:49 +0530

    Basic file initialization

---
+++ connection.php
@@ -0,0 +1,24 @@
+<?php
+
+    class Database{
+        public static $connection;
+        
+        public static function setUpConnection(){
+            if(!isset(Database::$connection)){
+                Database::$connection = new mysqli("localhost","root","project#123","film_booking_db","3306");
+            }
+        }
+
+        public static function insertUpdateDelete($q){
+            Database::setUpConnection();
+            Database::$connection->query($q);
+        }
+
+        public static function search($q){
+            Database::setUpConnection();
+            $resultset = Database::$connection->query($q);
+            return $resultset;
+        }
+    }
+
+?>
